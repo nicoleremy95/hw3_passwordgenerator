b2b67bd48ace7f5caa21fb5742f6cc6bed26a2cf
Nicole Remy <nicoleremy95@gmail.com>
2020-06-12 09:40:31 -0700

    completed password generator with while loop around the password length prompt

--- script.js
+++ script.js
@@ -9,29 +9,38 @@ function generatePassword() {
 
 var password = "";
   // after you ask the length prompt, you want the computer to limit the length of the password.
-  var passLength = prompt("how long would you like your password(no more than 120, no less than 8)?"); {
-    // 8<=passwordText=>128
-    // do {
-    if (8 <= passLength && passLength <= 128) {
-      console.log(passLength);
-    }
-    // if not a correct value
-    else {
-      console.log("value not valid");
-      alert("value not valid. choose a number between 8 and 128")
-    }
+  var passLength = prompt("how long would you like your password (no more than 120, no less than 8)?");
+  if (8 <= passLength && passLength <= 128) {
+    console.log(passLength);
   }
-  // while (8<=passLength && passLength<=120);
+  // if not a correct value
+  else {
+    console.log("value not valid");
+    alert("value not valid. choose a number between 8 and 128")
+  } 
+  // create while loop that says if the answer is invalid, try again. 
+    while (passLength<8 || passLength>128) {
+    var passLength = prompt("how long would you like your password(no more than 120, no less than 8)?"); 
+      // 8<=passwordText=>128
+    
+      if (8 <= passLength && passLength <= 128) {
+        console.log(passLength);
+      }
+      // if not a correct value
+      else {
+        console.log("value not valid");
+        alert("value not valid. choose a number between 8 and 128")
+      }
+    }
+  
 
   var passwordCharSet = [];
   // after you ask the charType, you want the computer to only use certain variables. using booleans 
   var yesLower = confirm("would you like lowercase?");
-  // if yes, include charLower
+  // if yes, start spread of array indexes
   if (yesLower) {
     passwordCharSet = [...charLower]
-    // passwordCharSet.push(charLower);
     // console.log(passwordCharSet);
-
   }
   // if no, do not include charLower
   else {
@@ -40,11 +49,9 @@ var password = "";
   var yesUpper = confirm("would you like uppercase?");
   // if yes, include charUpper
   if (yesUpper) {
+    // create spread that allows for the array to build on the previous array 
     passwordCharSet = [...passwordCharSet, ...charUpper]
-
-
     // console.log(charUpper);
-    // passwordCharSet.push(charUpper);
   }
   // if no, do not include charUpper
   else {
@@ -53,7 +60,7 @@ var password = "";
   var yesNum = confirm("would you like numbers?");
   // if yes, include num
   if (yesNum) {
-    create spread that 
+    // create spread that allows for the array to build on the previous array 
     passwordCharSet = [...passwordCharSet, ...num]
   }
   // if no, do not include num
@@ -63,34 +70,27 @@ var password = "";
   var yesSym = confirm("would you like symbols?");
   // if yes, include sym
   if (yesSym) {
+    // create spread that allows for the array to build on the previous array 
     passwordCharSet = [...passwordCharSet, ...sym]
     // console.log(sym);
-    // passwordCharSet.push(sym);
   }
+  // console log the resulting possible values of the password as an array 
   else {
     console.log("no symbols");
   }
   console.log(passwordCharSet);
-
+  // run for loop that sets i equal to the first index of your array passCharSet, that is less than the length that the user wants because that would result in the last index, and that keeps adding to itself. It also establishes the password equal to the password plus the value of the random set. this will output it as a string. 
   for (i = 0; i < passLength; i++) {
     password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
   }
+  // return the string value of the password 
   return password
 }
-// // set passwordCharSet var   
-// var passwordCharSet = [yesLower+yesUpper+yesNum+yesSym]
-// // mix array shuffle. let array equal passwordCharSet
-// function shuffle(passwordCharSet){
-// }
-//   // trim array .length
-// var passLength = passLength.length
-//   // use .join this turns array into string 
-// var newPassword = passLength();
-// }
 
 // PART 2
-
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -100,11 +100,5 @@ function writePassword() {
 
 }
 
-// PART 3
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
