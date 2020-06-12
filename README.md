775076eb8e42ab4f7ec4b9848f96eaf1460db46a
Nicole Remy <nicoleremy95@gmail.com>
2020-06-11 18:39:07 -0700

    completed homework not clean

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # hw3_passwordgenerator
 passwordgenerate
+
+In this project, I have created a password generator that will randomly choose a password given set criteria from the user. This will allow the user to use the resulting password to protect their sensitive data. 
+
+When you click the generate password button, it will prompt you to specify the length between 8-128 characters. After this, it will ask you to confirm yes or cancel to the following questions; do you want to use upper case characters?, do you want to use lowercase characters?, do you want to use numbers?, do you want to use symbols? Each of these questions results in an array of values that will ultimately be put into the function to create the password. 
+
+The resulting array will then by mixed, trimmed to the length specified by the user, and then the resulting array should be transferred into a string for the generate password function. 
+
+I created a loop that will continually keep spitting out different password options. 
+
--- script.js
+++ script.js
@@ -1,90 +1,109 @@
 // PART 1
 //variables 
-var charUpper ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var charLower ='abcdefghijklmnopqrstuvwxyz';
-var num ='0123456789';
-var sym ='!@#$%^&*=-_';
+var charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var sym = ["!", "@", "#", "$", "%", "^", "*", "'", "=", "-", "_"];
 
-function  generatePassword (){
+function generatePassword() {
 
-
-// after you ask the length prompt, you want the computer to limit the length of the password.Text.
-var passLength = prompt("how long would you like your password(no more than 120, no less than 8)?");{
-  // 8<=passwordText=>120
-  // do {
-    if (8<=passLength && passLength<=120){
+var password = "";
+  // after you ask the length prompt, you want the computer to limit the length of the password.
+  var passLength = prompt("how long would you like your password(no more than 120, no less than 8)?"); {
+    // 8<=passwordText=>128
+    // do {
+    if (8 <= passLength && passLength <= 128) {
       console.log(passLength);
     }
+    // if not a correct value
     else {
       console.log("value not valid");
-      alert("value not valid. choose a number between 8 and 120")
-   }
+      alert("value not valid. choose a number between 8 and 128")
+    }
   }
   // while (8<=passLength && passLength<=120);
 
-var passwordCharSet = " ";
-// after you ask the charType, you want the computer to only use certain variables. using booleans 
-var yesLower = confirm("would you like lowercase?");
+  var passwordCharSet = [];
+  // after you ask the charType, you want the computer to only use certain variables. using booleans 
+  var yesLower = confirm("would you like lowercase?");
   // if yes, include charLower
-    if (yesLower) {
-      console.log(charLower);
-      passwordCharSet += charLower;
-    }
+  if (yesLower) {
+    passwordCharSet = [...charLower]
+    // passwordCharSet.push(charLower);
+    // console.log(passwordCharSet);
+
+  }
   // if no, do not include charLower
-    else {
-      console.log("no lowercase");
-    }
-var yesUpper = confirm("would you like uppercase?");
+  else {
+    console.log("no lowercase");
+  }
+  var yesUpper = confirm("would you like uppercase?");
   // if yes, include charUpper
   if (yesUpper) {
-    console.log(charUpper);
-    passwordCharSet += charUpper
+    passwordCharSet = [...passwordCharSet, ...charUpper]
+
+
+    // console.log(charUpper);
+    // passwordCharSet.push(charUpper);
   }
   // if no, do not include charUpper
   else {
     console.log("no uppercase");
   }
-var yesNum = confirm ("would you like numbers?");
+  var yesNum = confirm("would you like numbers?");
   // if yes, include num
   if (yesNum) {
-    console.log(num);
-    passwordCharSet += num
+    create spread that 
+    passwordCharSet = [...passwordCharSet, ...num]
   }
   // if no, do not include num
   else {
     console.log("no numbers");
   }
-var yesSym = confirm ("would you like symbols?");
+  var yesSym = confirm("would you like symbols?");
   // if yes, include sym
   if (yesSym) {
-    console.log(sym);
-    passwordCharSet += sym
+    passwordCharSet = [...passwordCharSet, ...sym]
+    // console.log(sym);
+    // passwordCharSet.push(sym);
   }
-  // if no, do not include sym
   else {
     console.log("no symbols");
   }
+  console.log(passwordCharSet);
+
+  for (i = 0; i < passLength; i++) {
+    password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
+  }
+  return password
 }
+// // set passwordCharSet var   
+// var passwordCharSet = [yesLower+yesUpper+yesNum+yesSym]
+// // mix array shuffle. let array equal passwordCharSet
+// function shuffle(passwordCharSet){
+// }
+//   // trim array .length
+// var passLength = passLength.length
+//   // use .join this turns array into string 
+// var newPassword = passLength();
+// }
 
 // PART 2
+
 var generateBtn = document.querySelector("#generate");
-// WRITE PASSWORD TO THE #PASSWORD INPUT
-// computer should take the results from passLength and charType and run a function to give a password 
+// Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    // i want a random character for each slot of my passLength. This means I would do a mathrandom for each slot. 
-    var passwordText = passwordCharSet[Math.floor(Math.random()*passLength)];
-    // for loop
-    for (i=0; i<passLength; i++) {
-      userPassword += passwordCharSet.charAt(Math.floor(Math.random() * passLength));
-    }
-    passwordText.value = password;
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 }
 
-// ADD EVENT LISTENER TO GENERATE BUTTON
+// PART 3
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
 
 
 
